1a3776dd4b01fb9baeb8caee7275f0ea7e346f39
William Graham <w.m.graham24@gmail.com>
2018-03-13 14:11:08 +0000

    moved img to about me page

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
-console.log('loaded');
-console.log('rane is a wally');
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['page1', 'page2', 'page3', 'page4']
+  });
+  console.log('loaded');
+  console.log('rane is a wally');
+});
